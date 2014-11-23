149bbc3b81746b6407e7ef969f69b6f300f4fc51
Nicholas <nicholas.athera@gmail.com>
2014-11-23 12:20:05 -0500

    Update report.php

--- report.php
+++ report.php
@@ -1,5 +1,6 @@
 <?php
 include 'common.php';
+include $include_path . 'functions_cron.php';
 
 if (!$user->is_logged_in())
 {
@@ -14,7 +15,7 @@ if (!$user->is_logged_in())
   $res = mysql_query($query);
   $system->check_mysql($res, $query, __LINE__, __FILE__);
 	
-	$j=$i=0; 
+	$j=$i=$std_counter=$bno_counter=$dutch_counter=$std_counter_c=$bno_counter_c=$dutch_counter_c=0; 
 	while ($user_auctions_feed = mysql_fetch_array($res)){
     //for each auction get the info
     $id = $user_auctions_feed['id'];
@@ -24,75 +25,111 @@ if (!$user->is_logged_in())
   	$system->check_mysql($res2, $query, __LINE__, __FILE__);
   	$fees_total=0;
   	while ($fees = mysql_fetch_array($res2)){
+  		//echo '<br>'.$user_auctions_feed['title'].'='.$fees['total'];
+  		//echo '|'. $fees_total += $fees['total'];
   		$fees_total += $fees['total'];
   	}
   	$promo = ($user_auctions_feed['bold']=='y')?'B':'';
     $promo .= ($user_auctions_feed['highlighted']=='y')?'H':'';
     $promo .= ($user_auctions_feed['featured']=='y')?'F':'';
     
-    //maybe how profit/loss is calculated should be a user or admin option 
-    //current_bid-$fees-reserve_price  OR
-    //current_bid-$fees-minimum_bid  OR
-    //current_bid-$fees
-    //for now comment accordingly the 3 lines bellow
-     
-    if ($user_auctions_feed['reserve_price'] <= $user_auctions_feed['current_bid'] )
-    //$income = $user_auctions_feed['current_bid'] - $user_auctions_feed['reserve_price'] - $fees['total'];
-    //$income = $user_auctions_feed['current_bid'] - $user_auctions_feed['minimum_bid'] - $fees['total'];
-    	$income = $user_auctions_feed['current_bid'] - $fees_total;
-    else
-    	$income = 0 - $fees_total;
-
+    if ( $user_auctions_feed['bn_only']=='y' ){
+    	//echo '--auctionqty='.$user_auctions_feed['quantity'];
+    	
+    	
+      $query = "SELECT * FROM " . $DBPrefix . "winners WHERE auction =" . $id ;
+      $res3 = mysql_query($query);
+    	$system->check_mysql($res3, $query, __LINE__, __FILE__);
+      
+      $income=$quantity=$buyers = 0;
+      while ($winners = mysql_fetch_array($res3)){
+    		$income +=  $user_auctions_feed['buy_now'] * $winners['qty'];
+    		$quantity += $winners['qty'];
+    		$buyers++;
+    		//echo '<br>'.$user_auctions_feed['title'].'='.$quantity;
+    	}
+    	$income -=$fees_total;
+    }else if ($user_auctions_feed['auction_type']==2 ){	
+    	$dutch_info = get_lowest_qualified_bid($id ,$user_auctions_feed['quantity'],true);
+    	$income =  $dutch_info['lowest_qualified_bid'] * $dutch_info['quantity_sold'];
+    	$quantity = $dutch_info['quantity_sold'];
+    	$income -=$fees_total;
+		}else{
     
-    $rsrv_bgcolor = ($user_auctions_feed['reserve_price'] <= $user_auctions_feed['current_bid'] )?'bgcolor=lightgreen':'bgcolor=red';
-		$pofitloss_bgcolor = ($income >= 0 )?'bgcolor=lightgreen':'bgcolor=#F55';
+      //maybe how profit/loss is calculated should be a user or admin option 
+      //current_bid-$fees-reserve_price  OR
+      //current_bid-$fees-minimum_bid  OR
+      //current_bid-$fees
+      //for now comment accordingly 2 of the 3 lines bellow
+  		if ($user_auctions_feed['reserve_price'] <= $user_auctions_feed['current_bid'] )
+      //$income = $user_auctions_feed['current_bid'] - $user_auctions_feed['reserve_price'] - $fees['total'];
+      //$income = $user_auctions_feed['current_bid'] - $user_auctions_feed['minimum_bid'] - $fees['total'];
+      	$income = $user_auctions_feed['current_bid'] - $fees_total;
+      else
+      	$income = 0 - $fees_total;
+ 		}   
+    $rsrv_bgcolor = ($user_auctions_feed['reserve_price'] <= $user_auctions_feed['current_bid'] )?'bgcolor=lightgreen':'bgcolor=#F55';
+		if($user_auctions_feed['bn_only']!='y' && !$user_auctions_feed['closed'])
+			$pofitloss_bgcolor = ($income >= 0 )?'bgcolor=lightblue':'bgcolor=#F55';
+		else
+			$pofitloss_bgcolor = ($income >= 0 )?'bgcolor=lightgreen':'bgcolor=#F55';
 
     if ($user_auctions_feed['closed']){   //add data to the closed array
 
-    	$template->assign_block_vars('closed', array(
+			if ($user_auctions_feed['auction_type']==2){
+					$output_ra_name='dutch_c';
+					$j=$dutch_counter_c++;
+			}else if($user_auctions_feed['bn_only']=='y'){
+					$output_ra_name='bynowonly_c';
+					$j=$bno_counter_c++;
+			}else{
+					$output_ra_name='closed';
+					$j=$std_counter_c++;
+			}	
+			$closed_ra['total_income'] += $income;
+    	$closed_ra['total_fees'] += $fees_total;
+    	$closed_ra['total_num_views'] += $user_auctions_feed['counter'];
+    	$closed_ra['total_nun_bids'] += $user_auctions_feed['num_bids'];
+    	
+    }else{ //add data to the open array
+    	
+    	if ($user_auctions_feed['auction_type']==2){
+					$output_ra_name='dutch';
+					$j=$dutch_counter++;
+			}else if($user_auctions_feed['bn_only']=='y'){
+					$output_ra_name='bynowonly';
+					$j=$bno_counter++;
+			}else{
+    		$output_ra_name='open';   	
+    		$j= $i++;
+    	}
+    	$opened_ra['total_income'] += $income;
+    	$opened_ra['total_fees'] += $fees_total;
+    	$opened_ra['total_num_views'] += $user_auctions_feed['counter'];
+    	$opened_ra['total_nun_bids'] += $user_auctions_feed['num_bids'];   	
+    
+  	}
+  	
+  	$template->assign_block_vars($output_ra_name, array(
 			'BGCOLOUR' => (!(($j + 1) % 2)) ? '' : 'class="alt-row"',
 			'TITLE' => $user_auctions_feed['title'],
-			'STATUS' => 'Completed',
+			'STATUS' => ($user_auctions_feed['closed'])?'Completed':'Active',
 			'PROMO' => $promo,
 			'RSRV_BGCOLOR'=>$rsrv_bgcolor,
 			'POFITLOSS_BGCOLOR' => $pofitloss_bgcolor,
 			'VIEWS' => $user_auctions_feed['counter'],
 			'BIDS' => $user_auctions_feed['num_bids'],
 			'BID' => $user_auctions_feed['current_bid'],
+			'BUYERS' => $buyers,
+			'QUANTITY' => $quantity,
+			'PRICE' => $user_auctions_feed['buy_now'],
 			'RESERVE' => $user_auctions_feed['reserve_price'],
 			'FEES' => $fees_total,
 			'INCOME' => $income,
 			'ENDS' => ArrangeDateNoCorrection($user_auctions_feed['ends'] + $system->tdiff)
 			));
-			$j++;
-    	$closed_ra['total_income'] += $income;
-    	$closed_ra['total_fees'] += $fees_total;
-    	$closed_ra['total_num_views'] += $user_auctions_feed['counter'];
-    	$closed_ra['total_nun_bids'] += $user_auctions_feed['num_bids'];
-    	
-    }else{ //add data to the closed array
-    	    	
-    	$template->assign_block_vars('open', array(
-			'BGCOLOUR' => (!(($i + 1) % 2)) ? '' : 'class="alt-row"',
-			'TITLE' => $user_auctions_feed['title'],
-			'STATUS' => (time() < $user_auctions_feed['starts'] )?'Pending':'Live',
-			'PROMO' => $promo,
-			'RSRV_BGCOLOR'=>$rsrv_bgcolor,
-			'POFITLOSS_BGCOLOR' => $pofitloss_bgcolor,
-			'VIEWS' => $user_auctions_feed['counter'],
-			'BIDS' => $user_auctions_feed['num_bids'],
-			'BID' => $user_auctions_feed['current_bid'],
-			'RESERVE' => $user_auctions_feed['reserve_price'],
-			'FEES' => $fees_total,
-			'INCOME' =>  $income,
-			'ENDS' => ArrangeDateNoCorrection($user_auctions_feed['ends'] + $system->tdiff)
-			));
-			$i++;
-    	$opened_ra['total_income'] += $income;
-    	$opened_ra['total_fees'] += $fees_total;
-    	$opened_ra['total_num_views'] += $user_auctions_feed['counter'];
-    	$opened_ra['total_nun_bids'] += $user_auctions_feed['num_bids'];
-  	}
+  	
+  	
   } 
   
   $template->assign_vars(array(
